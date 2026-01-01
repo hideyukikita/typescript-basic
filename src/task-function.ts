e0c87bbd8892604768fd2a9412205d25cd48252a
HideyukiKita <kitatakikamo@gmail.com>
2026-01-01 17:40:47 +0900

    Fix:操作の修正

---
+++ src/task-function.ts
@@ -0,0 +1,22 @@
+// studentの型エイリアス
+type Student = {
+    name: string;
+    score: number;
+}
+
+const getResultMessage = (student: Student): string => {
+ if (student.score >= 60) {
+   return `${student.name}さんは合格です。`;
+ } else {
+   return `${student.name}さんは不合格です。`;
+ }
+};
+
+const studentA = {
+ name: '侍太郎',
+ score: 70,
+};
+
+let resultMessage = getResultMessage(studentA);
+
+console.log(resultMessage);
