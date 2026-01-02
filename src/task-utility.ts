5bddf33e3657c57ee175b633568fabaabc801bbf
HideyukiKita <kitatakikamo@gmail.com>
2026-01-02 16:40:50 +0900

    Add:課題対応

---
+++ src/task-utility.ts
@@ -0,0 +1,15 @@
+type Post = {
+ id: number;          // id
+ title: string;       // タイトル
+ body: string;        // 本文
+ published: boolean;  // 公開済みかどうか
+};
+
+// 編集中の投稿データの型
+type EmittingPost = Partial<Post>;
+
+// 一覧表示用データの型
+type SummaryPost = Pick<Post, 'id' | 'title'>;
+
+// 公開済みデータの型
+type PublishedPost = Readonly<Post>;
